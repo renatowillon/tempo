99514e50e61ae078ec30e9917451438e72417073
“renatowillon” <renatowillon@hotmail.com>
2024-07-30 14:19:34 -0300

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,99 @@
-import Image from "next/image";
+'use client'
+import { Toaster, toast } from 'sonner'
+import { FaSistrix, FaHeart, FaAngleDown, FaAngleUp } from "react-icons/fa6";
+
+const key = "2700b5defae477e83d3c60909f7c5276"
+
+
+// Alimentar Dados
+function dadosNaTela(dados){
+
+  const resCidade = document.querySelector("#rescidade");
+  const graus = document.querySelector("#graus");
+  const tempo = document.querySelector("#tempo");
+  const umidade = document.querySelector("#umidade");
+  const icontempo = document.querySelector("#icontempo");
+  const tmin = document.querySelector("#tmin");
+  const tmax = document.querySelector("#tmax");
+
+  if (resCidade) resCidade.innerHTML = `Previsão em ${dados.name}`;
+  if (graus) graus.innerHTML = `${Math.floor(dados.main.temp)} °C`;
+  if (tempo) tempo.innerHTML = dados.weather[0].description;
+  if (umidade) umidade.innerHTML = `Umidade ${dados.main.humidity}%`;
+  if (icontempo) icontempo.src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+  if (tmin) tmin.innerHTML = `${Math.floor(dados.main.temp_min)} °C`;
+  if (tmax) tmax.innerHTML = `${Math.floor(dados.main.temp_max)} °C`;
+
+}
+
+
+// Pegar Info Servidor
+async function buscarCidade(cidade){
+  
+  const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+  
+  dadosNaTela(dados)
+  console.log(dados)
+}
+
+// Pegar Cidade
+
+function buscar(){
+  const cidade = document.querySelector("#inpcidade")?.value
+  
+  if (cidade.length == 0){
+        toast.error('Preencha o nome da cidade')
+  }else{
+    //alert("CIDADE OK")
+    buscarCidade(cidade)
+  }
+
+  //
+
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    
+    <main className="w-screen h-screen flex-1 flex flex-col items-center justify-center bg-[url('https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+
+      <div id="conteudo" className="bg-slate-800/80 p-5 rounded-xl space-y-2 ">
+        
+        <div id="buscar" className="flex gap-3">
+          <input id="inpcidade" type="text" placeholder="Digite o nome da cidade" className="border-none outline-none bg-slate-600/80 p-2 rounded-full text-slate-400"/>
+          <button onClick={buscar} className="bg-slate-600/80 w-10 h-10 p-2 rounded-full flex items-center justify-center"><FaSistrix className="text-slate-400"/></button>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div id="rescidade" className="text-slate-200 py-3">Previsão do tempo</div>
+        <div className="flex justify-between col-span-5">
+        <div></div>
+          <div id="graus" className="text-slate-200 text-4xl font-semibold">00ºC</div>
+          <div>
+            <div id="min" className="text-slate-200 text-sm font-semibold flex items-center gap-1">MIN <FaAngleDown className="text-green-600"/></div>
+            <div id="tmin" className="text-slate-200 text-xs font-semibold flex items-center justify-center">00ºC</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div id="max" className="text-slate-200 text-sm font-semibold flex items-center gap-1">MAX <FaAngleUp className="text-red-600" /></div>
+            <div id="tmax" className="text-slate-200 text-xs font-semibold flex items-center justify-center">00ºC</div>
+          </div>
+          <div></div>
+        </div>
+        <div id="status" className="flex items-center justify-between">
+        <div></div>
+          <div className="flex items-center">
+            <img alt="icone info tempo" id="icontempo" src="https://openweathermap.org/img/wn/04n.png"></img>
+            <p id="tempo" className="text-slate-200 text-sm font-semibold capitalize">Climão</p>
+          </div>
+          
+          <p id="umidade" className="text-slate-200 text-xs font-semibold capitalize">Umidade: 00%</p>
+          <div></div>
+        </div>
+        
+      </div>        
+      
+      <div className="text-slate-800 text-xs font-semibold flex items-center gap-2 pt-2">feito com <FaHeart className="animate-pulse" /> por wDev</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
